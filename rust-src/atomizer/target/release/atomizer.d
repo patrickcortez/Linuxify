c69d0a64f526e15a5b04847eb7a8bee3c13aa70d
patrickcortez <patrickcortez2024@gmail.com>
2026-02-01 16:41:02 +0800

    removed test compiler

--- rust-src/atomizer/target/release/atomizer.d
+++
@@ -1,1 +0,0 @@
-c:\Users\patri\OneDrive\Documents\Projects\Linuxify\rust-src\atomizer\target\release\atomizer.exe: c:\Users\patri\OneDrive\Documents\Projects\Linuxify\rust-src\atomizer\src\main.rs
--- rust-src/atomizer/target/release/deps/atomizer.d
+++
@@ -1,5 +0,0 @@
-c:\Users\patri\OneDrive\Documents\Projects\Linuxify\rust-src\atomizer\target\release\deps\atomizer.d: src\main.rs
-
-c:\Users\patri\OneDrive\Documents\Projects\Linuxify\rust-src\atomizer\target\release\deps\atomizer.exe: src\main.rs
-
-src\main.rs:
