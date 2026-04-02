a4ce6ed53ec9596e14fbdac9ccae16d7ea88bb12
--Darkness-- <patrickcortez2024@gmail.com>
2026-04-02 12:22:27 +0800

    removing unnecessary components

--- .deprecated/fps-explorer/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-g++ -O2 -std=c++17 -mwindows -municode main.cpp -o fps-explorer.exe -lgdi32 -lshell32 -lole32 -lmsimg32
-if %ERRORLEVEL% EQU 0 (
-    echo Build successful: fps-explorer.exe
-) else (
-    echo Build failed.
-)
